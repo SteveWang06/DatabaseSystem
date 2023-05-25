5322d7a86244debcae3b42d35a786afe35486196
BradWithGithub <91183171+awrdrfs@users.noreply.github.com>
2023-05-25 21:05:44 +0800

    Update finalproject.sql
    設定not null跟預設值

--- finalproject.sql
+++ finalproject.sql
@@ -1,24 +1,24 @@
 CREATE DATABASE restaurant;
 SHOW DATABASES;
 
-DROP TABLE member;
+DROP TABLE orderpaper;
 
 CREATE TABLE customer(
-	customer_name varchar(30),
-	customer_phone varchar(10),
-	PRIMARY KEY (customer_name)
+	customer_name varchar(30) not null,
+    customer_phone varchar(10) not null,
+    PRIMARY KEY (customer_name)
 );
 
 CREATE TABLE member(
-	member_name varchar(30),
-    member_phone varchar(10),
-    member_score int(20),
-    member_level int(2),
+	member_name varchar(30) not null,
+    member_phone varchar(10) not null,
+    member_score int(20) default 0,
+    member_level int(2) default 1,
     member_birthday date,
     member_ID varchar(10),
-    member_password varchar(20),
+    member_password varchar(20) not null,
     member_nickname varchar(30),
-    member_account varchar(30),
+    member_account varchar(30) not null,
     PRIMARY KEY (member_ID)
     #,
     #FOREIGN KEY (member_phone) REFERENCES customer(customer_phone),
@@ -26,67 +26,69 @@ CREATE TABLE member(
 );
 
 CREATE TABLE employee(
-	employee_name varchar(30),
-    employee_phone varchar(10),
+	employee_name varchar(30) not null,
+    employee_phone varchar(10) not null,
     employee_living varchar(50),
-    employee_number varchar(5),
+    employee_number varchar(5) not null,
     PRIMARY KEY (employee_number)
 );
 
 CREATE TABLE history(
-	history_ID varchar(10),
-    history_time datetime,
-    history_content varchar(1000),
+	history_ID varchar(10) not null,
+    history_time datetime not null,
+    history_content varchar(1000) not null,
     PRIMARY KEY (history_ID)
 );
 
 CREATE TABLE kitchen(
-	ingredient_name varchar(30),
-	ingredient_number int(2),
-    ingredient_set varchar(10),
+	ingredient_name varchar(30) not null,
+	ingredient_number int(2) default 0,
+    ingredient_set varchar(10) not null,
     PRIMARY KEY (ingredient_name)
 );
 
 CREATE TABLE schedule(
-	employee_name int(30),
-    employee_number varchar(5),
-    workstart time,
-    workfinish time,
+	employee_name int(30) not null,
+    employee_number varchar(5) not null,
+    workstart time not null,
+    workfinish time not null,
     PRIMARY KEY (employee_name)
 );
 
 CREATE TABLE seat(
-	seat_number varchar(5),
-    seat_idle tinyint(1),
+	seat_number varchar(5) not null,
+    seat_idle tinyint(1) default 1,
+    #空閒=1,有人=0
     PRIMARY KEY (seat_number)
 );
 
 CREATE TABLE store(
-	store_number varchar(5),
-    store_living varchar(50),
-    store_open time,
-    store_close time,
+	store_number varchar(5) not null,
+    store_living varchar(50) not null,
+    store_open time not null,
+    store_close time not null,
     PRIMARY KEY (store_number)
 );
 
 CREATE TABLE pay(
-	pay_ID varchar(10),
-	pay_type varchar(10),
+	pay_ID varchar(10) not null,
+	pay_type varchar(10) not null,
     pay_discount varchar(10),
-    pay_complete tinyint(1),
-    pay_money int(5),
+    pay_complete tinyint(1) not null,
+    #已付款=1,未付款=0
+    pay_money int(5) not null,
     PRIMARY KEY (pay_ID)
 );
 
 CREATE TABLE orderpaper(
-	order_ID varchar(10),
-    order_time datetime,
-    order_content varchar(1000),
+	order_ID varchar(10) not null,
+    order_time datetime not null,
+    order_content varchar(1000) not null,
     PRIMARY KEY (order_ID)
 );
 
 SHOW TABLES;
-SHOW COLUMNS FROM pay;
+SHOW COLUMNS FROM customer;
 
 #insert into `member` values("張三", 0987654321, 80, 3, "1990-05-10", 1, "password1", "張三", "user1\@gmail.com")
 
