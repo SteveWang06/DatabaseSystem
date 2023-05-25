731a2c93427b7c8528d725bd37d9046deb813625
BradWithGithub <91183171+awrdrfs@users.noreply.github.com>
2023-05-25 21:53:20 +0800

    Update finalproject.sql
    將部分primary key更改為int auto_increment

--- finalproject.sql
+++ finalproject.sql
@@ -15,7 +15,7 @@ CREATE TABLE member(
     member_score int(20) default 0,
     member_level int(2) default 1,
     member_birthday date,
-    member_ID varchar(10),
+    member_ID int AUTO_INCREMENT, #AUTO_INCREMENT會以遞增數字自動填入id
     member_password varchar(20) not null,
     member_nickname varchar(30),
     member_account varchar(30) not null,
@@ -34,7 +34,7 @@ CREATE TABLE employee(
 );
 
 CREATE TABLE history(
-	history_ID varchar(10) not null,
+	history_ID int AUTO_INCREMENT,
     history_time datetime not null,
     history_content varchar(1000) not null,
     PRIMARY KEY (history_ID)
@@ -56,14 +56,14 @@ CREATE TABLE schedule(
 );
 
 CREATE TABLE seat(
-	seat_number varchar(5) not null,
+	seat_number int AUTO_INCREMENT,
     seat_idle tinyint(1) default 1,
     #空閒=1,有人=0
     PRIMARY KEY (seat_number)
 );
 
 CREATE TABLE store(
-	store_number varchar(5) not null,
+	store_number int AUTO_INCREMENT,
     store_living varchar(50) not null,
     store_open time not null,
     store_close time not null,
@@ -71,17 +71,17 @@ CREATE TABLE store(
 );
 
 CREATE TABLE pay(
-	pay_ID varchar(10) not null,
+	pay_ID int AUTO_INCREMENT,
 	pay_type varchar(10) not null,
     pay_discount varchar(10),
     pay_complete tinyint(1) not null,
     #已付款=1,未付款=0
-    pay_money int(5) not null,
+    pay_money int not null,
     PRIMARY KEY (pay_ID)
 );
 
 CREATE TABLE orderpaper(
-	order_ID varchar(10) not null,
+	order_ID int AUTO_INCREMENT,
     order_time datetime not null,
     order_content varchar(1000) not null,
     PRIMARY KEY (order_ID)
