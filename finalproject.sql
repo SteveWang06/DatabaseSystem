5c1cb78e5f9565844d0876d92d4cfa41f988e24d
BradWithGithub <91183171+awrdrfs@users.noreply.github.com>
2023-05-26 11:49:40 +0800

    Update finalproject.sql
    填了一點資料

--- finalproject.sql
+++ finalproject.sql
@@ -1,13 +1,20 @@
 CREATE DATABASE restaurant;
 SHOW DATABASES;
 
-DROP TABLE schedule;
+DROP TABLE kitchen;
 
 CREATE TABLE customer(
 	customer_name varchar(30) not null,
     customer_phone varchar(10) not null unique,
     PRIMARY KEY (customer_name,customer_phone)
 );
+
+insert into `customer` values('劉美玉','0911234567');
+insert into `customer` values('陳大偉','0922345678');
+insert into `customer` values('林小婷','0933456789');
+insert into `customer` values('林花綸','0912345678');
+insert into `customer` values('張美環','0923456789');
+insert into `customer` values('王小玉','0945678901');
 
 CREATE TABLE member(
 	member_name varchar(30) not null,
@@ -32,6 +39,10 @@ ADD FOREIGN KEY (member_name)
 REFERENCES customer(customer_name)
 ON DELETE NO ACTION;
 
+insert into `member` values('林花綸', '0912345678', 80, 3, "1990-05-10", 1, "password1", "AlanLin", "user1\@gmail.com");
+insert into `member` values('張美環', '0923456789', 50, 2, "1988-12-15", 2, "password2", "Mary1215", "user2\@gmail.com");
+insert into `member` values('王小玉', '0945678901', 2, 1, "1995-07-20", 3, "password3", "tina0720", "user3\@gmail.com");
+
 CREATE TABLE employee(
 	employee_name varchar(30) not null,
     employee_phone varchar(10) not null,
@@ -39,6 +50,11 @@ CREATE TABLE employee(
     employee_number varchar(5) not null,
     PRIMARY KEY (employee_number)
 );
+
+insert into `employee` values('張三','0912345678','台中市北屯區文心路四段57號','C0001');
+insert into `employee` values('李四','0923456789','台中市南屯區中和里黎明路一段263號','C0002');
+insert into `employee` values('陳小美','0987654321','台中市北屯區橫坑巷28-1號','W0001');
+insert into `employee` values('張大志','0976543210','台中市西屯區至善路61號','W0002');
 
 CREATE TABLE history(
 	history_ID int AUTO_INCREMENT,
@@ -54,10 +70,14 @@ ON DELETE NO ACTION;
 
 CREATE TABLE kitchen(
 	ingredient_name varchar(30) not null,
-	ingredient_number int(2) default 0,
+	ingredient_number int default 0,
     ingredient_set varchar(10) not null,
     PRIMARY KEY (ingredient_name)
 );
+
+insert into `kitchen` values('雞胸肉',500,'公克');
+insert into `kitchen` values('蔬菜沙拉',1,'包');
+insert into `kitchen` values('鮮奶油',200,'毫升');
 
 CREATE TABLE schedule(
 	employee_name varchar(30) not null,
@@ -77,6 +97,10 @@ ADD FOREIGN KEY (employee_name)
 REFERENCES employee(employee_name)
 ON DELETE NO ACTION; 
 
+insert into `schedule` values('張三','C0001','09:00:00','17:00:00');
+insert into `schedule` values('李四','C0002','13:00:00','21:00:00');
+insert into `schedule` values('陳小美','W0001','10:00:00','18:00:00');
+insert into `schedule` values('張大志','W0002','10:00:00','16:00:00');
 
 CREATE TABLE seat(
 	seat_number int AUTO_INCREMENT,
@@ -85,6 +109,10 @@ CREATE TABLE seat(
     PRIMARY KEY (seat_number)
 );
 
+insert into `seat` values(1,1);
+insert into `seat` values(2,0);
+insert into `seat` values(3,1);
+
 CREATE TABLE store(
 	store_number int AUTO_INCREMENT,
     store_living varchar(50) not null,
@@ -92,6 +120,10 @@ CREATE TABLE store(
     store_close time not null,
     PRIMARY KEY (store_number)
 );
+
+insert into `store` values(1,'台北市中山區明水路581巷15號1樓','09:00:00','21:00:00');
+insert into `store` values(2,'新北市板橋區文化路二段227號、229號1樓','10:00:00','20:00:00');
+insert into `store` values(3,'台中市西屯區中平路279號','11:00:00','22:00:00');
 
 CREATE TABLE pay(
 	pay_ID int AUTO_INCREMENT,
@@ -124,7 +156,5 @@ ON DELETE NO ACTION;
 SHOW TABLES;
 SHOW COLUMNS FROM customer;
 
-#insert into `member` values("張三", 0987654321, 80, 3, "1990-05-10", 1, "password1", "張三", "user1\@gmail.com")
-
 select *
-from member
+from schedule
